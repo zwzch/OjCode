98eb4a280e607b9191a1d9fa345d28c857cd5209
zwzch <1768259692@qq.com>
2018-09-23 20:28:58 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include "sstream"
+using namespace std;
+extern void Test();
+bool Find(int target, vector<vector<int> > array);
+void replaceSpace(char *str,int length);
+extern int minNumberInRotateArray(vector<int> rotateArray);
+extern int Fibonacci(int n);
+extern int jumpFloor(int number);
+extern int jumpFloorII(int number);
+extern int rectCover(int number);
+extern void reOrderArray(vector<int> &array);
+int main() {
+//    string str = "hello world";
+//    char * strc = const_cast<char *>(str.c_str());
+//    cout<<strc<<endl;
+//    replaceSpace(strc,11);
+//    int a[] = {3,4,5,1,2};
+//    vector<int > arr(a, a+5);
+//    cout<<minNumberInRotateArray(arr)<<endl;
+//    cout<<Fibonacci(3)<<endl;
+//    cout<<jumpFloorII(3)<<endl;
+//    cout<<rectCover(3)<<endl;
+//    double number = 0.25;
+//    reOrderArray(arr);
+//    for (int i = 0; i < arr.size(); ++i) {
+//        cout<<arr[i]<<endl;
+//    }
+    Test();
+    return 0;
+}
