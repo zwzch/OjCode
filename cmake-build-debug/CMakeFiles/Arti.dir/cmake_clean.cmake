ea98ac90a62fd37c759b629b97b1fe18fab5588b
zwzch <1768259692@qq.com>
2018-09-25 09:15:45 +0800

    x

---
+++ cmake-build-debug/CMakeFiles/Arti.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Arti.dir/main.cpp.o"
+  "CMakeFiles/Arti.dir/jianzhi_offer.cpp.o"
+  "Arti.pdb"
+  "Arti"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arti.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
